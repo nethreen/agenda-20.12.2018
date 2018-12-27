00b6a4223a2ca9af670cbd9e85047e908353c3ad
Soltanli Nazrin Rovshen  <Sola_uy20@itstep.edu.az>
2018-12-27 12:19:11 +0400

    test

---
+++ main.js
@@ -0,0 +1,7 @@
+// document.getElementById("menu-btn").onclick = function(){
+//     if(document.getElementById("mobile-menu").classList.contains("open-menu")){
+//         document.getElementById("mobile-menu").classList.remove("open-menu");
+//     }else{
+//         document.getElementById("mobile-menu").classList.add("open-menu");
+//     }
+// }
